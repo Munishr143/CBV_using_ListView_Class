cd40181beec6690ad52fa4c6183cac3daefa792c
Munishr143 <mm1437799@gmail.com>
2023-05-20 11:33:34 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from app.models import *
 
 class Home(TemplateView):
@@ -10,3 +10,11 @@ class Home(TemplateView):
 class School_List(ListView):
     model=School
     context_object_name='schools'
+
+class SchoolDetail(DetailView):
+    model=School
+    context_object_name='sclobject'
+
+class Create_School(CreateView):
+    model=School
+    fields='__all__'
